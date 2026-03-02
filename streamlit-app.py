d976c1ff02133c232f8e95ea90336e661d39e24c
kwoneunsuk <lemon91212@gmail.com>
2026-03-02 23:30:21 +0700

    Add files via upload

--- streamlit-app.py
+++ streamlit-app.py
@@ -5,46 +5,45 @@ import joblib
 # -----------------------
 # 페이지 설정
 # -----------------------
-st.header("신체 정보를 이용한 키 예측 머신러닝 모델")
-st.write("신체 정보를 입력하면 키를 예측합니다.")
+st.title("신체 정보를 이용한 몸무게 예측 머신러닝 모델")
+st.write("신체 정보를 입력하면 몸무게를 예측합니다.")
 
 # -----------------------
 # 모델 로드 (한 번만)
 # -----------------------
-model_male = joblib.load("height_model_male.pkl")
-model_female = joblib.load("height_model_female.pkl")
+model_male = joblib.load("weight_model_male.pkl")
+model_female = joblib.load("weight_model_female.pkl")
 
 # -----------------------
 # 성별 선택
 # -----------------------
 gender = st.radio("성별 선택", ["남자", "여자"])
 
-st.sidebar.header("머신러닝 모델 설계 실습")
+st.sidebar.header("머신러닝 모델 설계 실습 (다중회귀)")
 
 # -----------------------
 # 입력 UI + 모델 선택
 # -----------------------
 if gender == "남자":
-    weight = st.slider("몸무게 (kg)", 30.0, 150.0, 60.0)
-    hip = st.slider("엉덩이 둘레 (cm)", 60.0, 150.0, 90.0)
-    shoe = st.slider("신발 치수 (mm)", 200, 320, 260)
+    height = st.slider("키 (cm)", 140.0, 190.0, 170.0)
+    chest = st.slider("가슴 둘레 (cm)", 80.0, 120.0, 90.0)
+    waist = st.slider("허리 둘레 (cm)", 50.0, 120.0, 80.0)
+    waist2 = st.slider("엉덩이 둘레 (mm)", 85, 120, 100)
 
-    X = np.array([[weight, hip, shoe]])
+    X = np.array([[height, chest, waist, waist2]])
     model = model_male
 
 else:
-    weight = st.slider("몸무게 (kg)", 30.0, 150.0, 60.0)
+    waist = st.slider("허리 둘레 (cm)", 40.0, 120.0, 80.0)
     neck = st.slider("목 둘레 (cm)", 20.0, 50.0, 30.0)
-    hip = st.slider("엉덩이 둘레 (cm)", 60.0, 150.0, 90.0)
+    hip = st.slider("엉덩이 둘레 (cm)", 80.0, 120.0, 90.0)
 
-    X = np.array([[weight, neck, hip]])
+    X = np.array([[waist, neck, hip]])
     model = model_female
 
 # -----------------------
 # 예측
 # -----------------------
-if st.button("키 예측하기"):
+if st.button("몸무게 예측하기"):
     prediction = model.predict(X)
-    st.success(f"예측 키: {prediction[0]:.1f} cm")
-
-
+    st.success(f"예측 몸무게 : {prediction[0]:.1f} kg")
